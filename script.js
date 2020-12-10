ea1a34f5733033efbb8d61a6158f10bc0e5d58b2
Matt Spence <Matthew@Matts-MacBook-Pro-9.local>
2020-12-10 18:07:58 -0500

    First Commit Chapter

---
+++ script.js
@@ -0,0 +1,33 @@
+if(1 + 1 >5){
+    console.log('True fact!')
+} else {
+    console.log('Lies!')
+;}
+if(4 * 5 <= 20){
+    console.log('True fact!')
+} else {
+    console.log('Lies!')
+;}
+if(6 - 2 >= 0){
+    console.log('True fact!')
+} else {
+    console.log('Lies!')
+;}
+
+likeDogs = true
+if(likeDogs === true){
+    console.log("You're a dog person!")
+} else{
+    console.log("You're a cat person!")
+};
+
+let birthYear = 1995
+birthYear = 1986
+
+if(birthYear >= 1995 && birthYear <= 2012){
+    console.log('You are apart of Generation Z!')
+} else {
+    console.log('You are not apart of the this generation...');
+};
+ 
+
